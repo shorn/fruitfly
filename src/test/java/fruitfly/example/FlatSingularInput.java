fad47c1ab2f21df11d08dd44088ac3be476009f6
Shorn <bitbucket-shorn@kopi.cloud>
2024-02-10 16:24:11 +1000

    remove unused classes

--- src/test/java/fruitfly/example/FlatSingularInput.java
+++
@@ -1,9 +0,0 @@
-package fruitfly.example;
-
-/**
- The simplest singular top level record that we would want to generate
- a builder for.
- */
-public record FlatSingularInput(String field1, String field2) {
-
-}
